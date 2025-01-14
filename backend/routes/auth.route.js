48998fff262e18abf1efccf088ad972724e4fc52
ThamiBa <baladithami@gmail.com>
2025-01-14 15:51:28 +0100

    user-auth-system

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,4 +6,12 @@ router.get("/SignUp", (req, res) => { // Create route for GET request
     res.send("SignUp route!"); // Send response
 });
 
+router.get("/Login", (req, res) => { // Create route for GET request
+    res.send("Login route!"); // Send response
+});
+
+router.get("/Logout", (req, res) => { // Create route for GET request
+    res.send("Logout route!"); // Send response
+});
+
 export default router;	// Export router
