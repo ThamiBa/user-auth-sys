265b020732d9a326a42a6c001201c1343584b168
ThamiBa <baladithami@gmail.com>
2025-01-15 00:27:43 +0100

    user-auth-system

---
+++ backend/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    lastlogin: {
+        type: Date,
+        default: Date.now,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    resetPasswordToken: string,
+    resetPasswordExpiresAt: Date,
+    verificationToken: string,
+    verificationTokenExpiresAt: Date,
+}, { timestamps: true });
