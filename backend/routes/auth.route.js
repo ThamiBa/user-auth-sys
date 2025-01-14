15318a4aa4805237f8d0d9413cd9b821369260ea
ThamiBa <baladithami@gmail.com>
2025-01-14 15:47:12 +0100

    user-auth-system

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,4 +2,8 @@ import express from 'express'; // Import express
 
 const router = express.Router();	// Create router
 
+router.get("/", (req, res) => { // Create route for GET request
+    res.send("SignUp route!"); // Send response
+});
+
 export default router;	// Export router
