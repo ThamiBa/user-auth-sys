56608a76a7c1204265ff3d25eded67110c9e64d9
ThamiBa <baladithami@gmail.com>
2025-01-14 15:37:53 +0100

    user-auth-system

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'; // Import mongoose
 export const connectDB = async () => { // Async function to connect to MongoDB
     try { // Try to connect to MongoDB
-        const conn = await mongoose.connect(process.env.MONGO_URI) // Connect to MongoDB
-        console.log(`MongoDB Connected: ${conn.connection.host}`) // Log connection host
+        console.log("mongo_uri: ", process.env.MONGO_URI); // Log MongoDB URI
+        const conn = await mongoose.connect(process.env.MONGO_URI); // Connect to MongoDB
+        console.log(`MongoDB Connected: ${conn.connection.host}`); // Log connection host
     } catch (error) {
-        console.log("Error connecting to MongoDB: ", error.message) // Log error message
-        process.exit(1) // Exit with failure
+        console.log("Error connecting to MongoDB: ", error.message); // Log error message
+        process.exit(1) // 1 is failure, 0 status code is success
     }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from 'express';	// Import express
+import dotenv from 'dotenv';	// Import dotenv
+
+import { connectDB } from './db/connectDB.js'; // Import connectDB function
+
+dotenv.config();	// Load environment variables
 
 const app = express();	// Create express app
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => { // Create route for GET request
+    res.send("Hello World!"); // Send response
 });
 
+app.use("/api/auth", authRoutes); // Use authRoutes for /api/auth route
+
 app.listen(3000, () => {	// Start server
-    console.log('Server is running on port 3000');
+    connectDB(); // Connect to MongoDB
+    console.log('Server is running on port 3000'); // Log message
 });
