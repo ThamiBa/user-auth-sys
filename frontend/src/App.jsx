1e0973e56e47fd14f5494b41172e462b03dd9401
ThamiBa <baladithami@gmail.com>
2025-01-19 16:18:15 +0100

    user-auth-system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1 className='text-red-500 text-5xl'>Hello World!</h1>
-    </>
+    <div className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 
+    flex items-center justify-center relative overflow-hidden'>
+      
+    </div>
   )
 }
 
