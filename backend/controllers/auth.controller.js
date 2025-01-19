53fc4a571875147feb7fb64b5aa791c542d96c6f
ThamiBa <baladithami@gmail.com>
2025-01-19 03:56:28 +0100

    user-auth-system

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -188,13 +188,17 @@ export const resetPassword = async (req, res) => { // Reset password function: H
 
 export const checkAuth = async (req, res) => { // Check auth function: Handles checking the user's authentication status
     try {
-        const user = await User.findById(req.userId); // Find a user by the user ID in the request object
+        const user = await User.findById(req.userId).select("-password"); // Find a user by the user ID in the request object
         if (!user) { // If the user does not exist, return a 400 error with a message
             return res.status(400).json({ success: false, message: "User not found" });
         }
-        res.status(200).json({ success: true, user }); // Respond with a success message and the user data
+        res.status(200).json({ success: true, user: {  // Respond with a success message and the user data (excluding the password)
+                ...user._doc,
+                password: undefined,
+            }
+         });
     } catch (error) {
         console.log("error in checkAuth ", error);
         res.status(400).json({ success: false, message: error.message });
     }
-}
+} 
