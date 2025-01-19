808d8defa06f9750f5c6216ecd97e3d5caac22ff
ThamiBa <baladithami@gmail.com>
2025-01-19 18:42:38 +0100

    user-auth-system

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,6 +1,16 @@
-const FloatingShape = () => {
+import { motion } from 'framer-motion'
+
+const FloatingShape = (color, size, top, left, delay) => {
   return (
-    <div>FloatingShape</div>
+    <motion.div
+        className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl ${top} ${left}`}
+        animate={{ 
+            y: ["0%", "100%", "0%"],
+            x: ["0%", "100%", "0%"],
+            rotate: [0, 360],
+        }}
+        transition={{ duration: 3, delay: delay, repeat: Infinity }}
+    >FloatingShape</motion.div>
   )
 }
 
