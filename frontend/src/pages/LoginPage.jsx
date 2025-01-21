11c014cf047fe8076640f661279b7bc6c5dac5b7
ThamiBa <baladithami@gmail.com>
2025-01-21 13:34:58 +0100

    user-auth-system

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import { Mail, Lock, Loader } from 'react-feather'
+import { Link } from 'react-router-dom'
+
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+    }
+
     return (
-      <div>
-        
-      </div>
+      <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl
+        rounded-2xl shadow-xl overflow-hidden'
+      >
+        <div className='p-8'>
+
+        </div>
+      </motion.div>
     )
   }
   
