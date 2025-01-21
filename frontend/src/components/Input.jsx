cd8296a7e620c9bf223a43688e3f84f67adc5c22
ThamiBa <baladithami@gmail.com>
2025-01-21 12:11:21 +0100

    user-auth-system

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -6,7 +6,9 @@ const Input = ({ icon: Icon, ...props }) => {
 		</div>
 		<input
 		  {...props}
-		  className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-transparent focus:ring-2 focus:ring-transparent focus:bg-gradient-to-r from-indigo-500 to-purple-500 text-white placeholder-gray-400 transition duration-200'
+		  className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border
+		  border-gray-700 focus:border-transparent focus:ring-2 focus:ring-transparent focus:bg-gradient-to-r
+		  from-indigo-500 to-purple-500 text-white placeholder-gray-400 transition duration-200'
 		/>
 	  </div>
 	);
