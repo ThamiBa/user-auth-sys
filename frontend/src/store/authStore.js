0bb0226125fd5d80c6f77896e45ca30c72e83308
ThamiBa <baladithami@gmail.com>
2025-01-22 10:45:43 +0100

    user-auth-system

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -6,6 +6,8 @@ import EmailVerification from '../pages/EmailVerificationPage';
 
 const API_URL = 'http://localhost:5000/api/auth';
 
+axios.defaults.withCredentials = true;
+
 export const useAuthStore = create((set) => ({
     user: null,
     isAuthenticated: false,
