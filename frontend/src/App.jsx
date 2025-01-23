81c02bb703cca812af7d510bb2baa6304a405178
ThamiBa <baladithami@gmail.com>
2025-01-23 06:58:29 +0100

    user-auth-system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,15 @@ import SignUpPage from "./pages/SignUpPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
 
 import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from "./store/authStore.js";
+import { useEffect } from "react";
 
 function App() {
+  const {isCheckingAuth,checkAuth} = useAuthStore();
+
+  useEffect(() => {
+
+  },[checkAuth]);
   return (
     <div className='min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-indigo-900 flex items-center justify-center relative overflow-hidden'>
       <FloatingShape color='bg-blue-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
