6b1bc206b8111f1896ed99032cb5ba0266f92dcb
ThamiBa <baladithami@gmail.com>
2025-01-25 13:49:23 +0100

    user-auth-system

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -27,6 +27,27 @@ const DashboardPage = () => {
 					<p className='text-gray-300'>Name: {user.name}</p>
 					<p className='text-gray-300'>Email: {user.email}</p>
 				</motion.div>
+        <motion.div
+					className='p-4 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700'
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ delay: 0.4 }}
+				>
+					<h3 className='text-xl font-semibold text-green-400 mb-3'>Account Activity</h3>
+					<p className='text-gray-300'>
+						<span className='font-bold'>Joined: </span>
+						{new Date(user.createdAt).toLocaleDateString("en-US", {
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						})}
+					</p>
+					<p className='text-gray-300'>
+						<span className='font-bold'>Last Login: </span>
+
+						{user.lastLogin ? formatDate(user.lastLogin) : "You just signed up!"}
+					</p>
+				</motion.div>
       </div>
     </motion.div>
   )
